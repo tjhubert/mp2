ab7484737cf3a8a1b60ec279317ad894022faabd
Hubert Theodore <hubert@yup.com>
2016-02-25 23:26:45 -0600

    changed a lot of stuff

--- source_js/detect_browser.js
+++ source_js/detect_browser.js
@@ -2,10 +2,13 @@ var isPhone = false;
 (function() {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
         // var person = prompt("Please use a laptop or desktop computer to access this website.", "");
-        alert("For best viewing please consider using your laptop or desktop computer.");
+        alert("For even more cool pop art magic, please use your laptop or desktop computer.");
         isPhone = true;
     }
+    else if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1){
+        alert("For even more cool pop art magic, please use Firefox or Chrome ")
+    }
     else {
-        // alert("Please .");
+        alert("For even more cool pop art magic, please allow webcam access.");
     }
 })();
--- source_js/multi-column.js
+++ source_js/multi-column.js
@@ -1,80 +1,35 @@
 (function () {
     var photoVideoContainerElems = $(".input-photo-video");
     var multiColumnElem = $("#multi-column");
-    var failGetWebcam = false;
 
-    // if (isPhone) {
-    //     multiColumnElem.append('<input type="file" id="take-picture" accept="image/*">');
-    //     var takePicture = document.querySelector("#take-picture");
-    //     takePicture.onchange = function (event) {
-    //         // Get a reference to the taken picture or chosen file
-    //         var files = event.target.files,
-    //             file;
-    //         if (files && files.length > 0) {
-    //             file = files[0];
-    //         }
-    //     };
-    // }
-
-     function appendImageVideoElems() {
+     function replaceImageWithWebcam() {
         Array.prototype.forEach.call(photoVideoContainerElems, function(elem){
             var $elem = $(elem);
-            if (isPhone || failGetWebcam) {
-                $elem.prepend('<img src="./data/images/warhol_portrait.jpg" alt="portrait">');
-            }
-            else {
-                $elem.prepend('<video class="column-webcam"></video>');
-            }
+            $elem.find(".filter-overlay").replaceWith('<video class="filter-overlay column-webcam"></video>');
         });
     }
 
-    if (isPhone) {
-        appendImageVideoElems();
-        // console.log("this is phone");
-
-        // var photoImgElems = $(".show-picture");
-
-        // // Create ObjectURL
-        // var imgURL = window.URL.createObjectURL(file);
-
-        // Array.prototype.forEach.call(photoImgElems, function(elem){
-        //     var $elem = $(elem);
-        //     $elem.attr("src", imgURL);
-        // });
-
-        // // Set img src to ObjectURL
-        // showPicture.src = imgURL;
-
-        // // For performance reasons, revoke used ObjectURLs
-        // URL.revokeObjectURL(imgURL);
-    }
-    else {
+    if (!isPhone) {
         navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia);
 
-        if (navigator.getMedia) {
-            navigator.getMedia(
-                // constraints
-                {video:true, audio:false},
+        navigator.getMedia(
+            // constraints
+            {video:true, audio:false},
 
-                // success callback
-                function (mediaStream) {
-                    appendImageVideoElems();
-                    var videos = document.getElementsByClassName('column-webcam');
-                    Array.prototype.forEach.call(videos, function(video){
-                        video.src = window.URL.createObjectURL(mediaStream);
-                        video.play();
-                    });
-                },   
-                //handle error
-                function (error) {
-                    failGetWebcam = true;
-                    console.log(error);
-                    appendImageVideoElems();
+            // success callback
+            function (mediaStream) {
+                replaceImageWithWebcam();
+                var videos = document.getElementsByClassName('column-webcam');
+                Array.prototype.forEach.call(videos, function(video){
+                    video.src = window.URL.createObjectURL(mediaStream);
+                    video.play();
                 });
-        } else {
-            failGetWebcam = true;
-            appendImageVideoElems();
-        }
+            },   
+            //handle error
+            function (error) {
+                console.log(error);
+            });
+        
     }
 
 
